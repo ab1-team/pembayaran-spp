2e90dee96eb27f2665e7b15aeb40f3369b3f9711
farilraehan <farilraihan50@gmail.com>
2026-08-07 11:23:42 +0700

    tampilan detail

--- resources/views/transaksi/map_arsip/detail.blade.php
+++ resources/views/transaksi/map_arsip/detail.blade.php
@@ -9,7 +9,7 @@
                 <div class="table-responsive">
                     <table id="keuangan" class="table align-items-center table-striped">
                         <thead>
-                            <tr>
+                            <tr align="center">
                                 <th width="6%">ID</th>
                                 <th width="12%">Tanggal Trx</th>
                                 <th width="14%">Kode Akun</th>
@@ -20,29 +20,32 @@
                         </thead>
                         <tbody>
                             @forelse ($siswa->getTransaksi as $item)
-<tr>
-                                    <td>{{ $item->id }}</td>
-                                    <td>
+                                <tr>
+                                    <td align="center">{{ $item->id }}</td>
+                                    <td align="center">
+
                                         {{ Tanggal::tglIndo($item->tanggal_transaksi) }}
                                     </td>
-                                    <td>
+                                    <td align="center">
                                         <div class="small">
                                             <div>D: {{ $item->rekeningDebit->kode_akun ?? '-' }}</div>
                                             <div>K: {{ $item->rekeningKredit->kode_akun ?? '-' }}</div>
                                         </div>
                                     </td>
                                     <td>{{ $item->keterangan }}</td>
-                                    <td class="text-end">
+                                    <td align="right" style="padding-right: 10px;">
                                         {{ \App\Utils\Angka::format($item->getRawOriginal('jumlah'), 0) }}
                                     </td>
                                     <td class="text-center text-nowrap">
                                         <div class="d-inline-flex gap-1">
                                             <a href="/app/transaksi/kwitansi-spp?ids={{ $item->id }}"
-                                                target="_blank" class="btn btn-secondary btn-compact" title="Cetak Kwitansi">
+                                                target="_blank" class="btn btn-secondary btn-compact"
+                                                title="Cetak Kwitansi">
                                                 <i class="material-symbols-rounded">print</i>
                                             </a>
                                             <a href="/app/transaksi/cetakPadaKartu?ids={{ $item->id }}"
-                                                target="_blank" class="btn btn-secondary btn-compact" title="Cetak Pada Kartu">
+                                                target="_blank" class="btn btn-secondary btn-compact"
+                                                title="Cetak Pada Kartu">
                                                 <i class="material-symbols-rounded">credit_card</i>
                                             </a>
                                             <button type="button" class="btn btn-danger btn-compact btnDelete"
