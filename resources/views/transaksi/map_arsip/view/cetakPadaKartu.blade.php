bc27b6644d94f21c0863724e0df60991ec06f2b4
nurfandi31 <fandinur241@gmail.com>
2026-08-19 08:59:40 +0700

    tgl kartu

--- resources/views/transaksi/map_arsip/view/cetakPadaKartu.blade.php
+++ resources/views/transaksi/map_arsip/view/cetakPadaKartu.blade.php
@@ -55,7 +55,7 @@
         @foreach ($transaksis as $i => $trx)
             <tr>
                 <td width="7%" class="center">{{ $jumlahTransaksi + $i + 1 }}</td>
-                <td width="30%" class="center">{{ Tanggal::tglIndo($trx->tanggal) }}</td>
+                <td width="30%" class="center">{{ Tanggal::tglIndo($trx->tanggal_transaksi) }}</td>
                 <td style="padding-left:15px;">
                     @if ($trx->spp)
                         {{ Tanggal::namabulan($trx->spp->tanggal) }}
